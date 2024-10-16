7bd314e2701bcf14a91b7d4566941faf189e4a60
Michael Tanczos <tanczosm@eastonsd.org>
2024-10-16 08:23:23 -0400

    chore: Updated to decimal type

--- PiEstimator/PiEstimator/Program.cs
+++ PiEstimator/PiEstimator/Program.cs
@@ -1,48 +1,47 @@
 ﻿using System;
 
-namespace PiEstimator
+namespace PiEstimator;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        long n;
+        
+        Console.WriteLine("Pi Estimator");
+        Console.WriteLine("================================================");
+
+        n = GetNumber("Enter number of iterations (n): ");
+
+        decimal pi = EstimatePi(n);
+        decimal diff = Decimal.Abs(pi - (decimal)Math.PI);
+        
+        Console.WriteLine();
+        Console.WriteLine($"Pi (estimate): {pi}, Pi (system): {Math.PI}, Difference: {diff}");
+    }
+
+    static decimal EstimatePi(long n)
+    {
+        Random rand = new Random(System.Environment.TickCount);
+        decimal pi = 0.0m; // m means decimal number type
+
+        // TODO: Calculate Pi
+
+        return pi;
+    }
+
+    static long GetNumber(string prompt)
+    {
+        long output;
+
+        while (true)
         {
-            long n;
-            
-            Console.WriteLine("Pi Estimator");
-            Console.WriteLine("================================================");
+            Console.Write(prompt);
+            string input = Console.ReadLine();
 
-            n = GetNumber("Enter number of iterations (n): ");
-
-            double pi = EstimatePi(n);
-            double diff = Math.Abs(pi - Math.PI);
-
-            Console.WriteLine();
-            Console.WriteLine($"Pi (estimate): {pi}, Pi (system): {Math.PI}, Difference: {diff}");
-        }
-
-        static double EstimatePi(long n)
-        {
-            Random rand = new Random(System.Environment.TickCount);
-            double pi = 0.0;
-
-            // TODO: Calculate Pi
-
-            return pi;
-        }
-
-        static long GetNumber(string prompt)
-        {
-            long output;
-
-            while (true)
+            if (Int64.TryParse(input, out output))
             {
-                Console.Write(prompt);
-                string input = Console.ReadLine();
-
-                if (Int64.TryParse(input, out output))
-                {
-                    return output;
-                }
+                return output;
             }
         }
     }
